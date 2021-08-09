42a67616d4b47a9a73add4c19a3030ffa08c2523
bvinson23 <brandonvinson23@gmail.com>
2021-08-09 18:31:32 -0500

    contact model setup

---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Management_App.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+    }
+}
