76e10b6e0f17c3d8c63698746bafcf37b3b8f5cf
bvinson23 <brandonvinson23@gmail.com>
2021-08-09 18:37:11 -0500

    models set up for note, priority, task, and projectcontact

---
+++ Models/Priority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Management_App.Models
+{
+    public class Priority
+    {
+        public int Id { get; set; }
+        public string Level { get; set; }
+    }
+}
