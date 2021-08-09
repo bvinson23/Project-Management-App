f2c6163418493a84cc96883c2d6b01d10ffb2b98
bvinson23 <brandonvinson23@gmail.com>
2021-08-09 18:39:34 -0500

    models setup for projectnote and tasknote, models complete

---
+++ Models/ProjectNote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Management_App.Models
+{
+    public class ProjectNote
+    {
+        public int Id { get; set; }
+        public int NoteId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
