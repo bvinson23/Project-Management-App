ef1dc5018fa0e5bdbf2cc42f0292e8adb1c7c940
bvinson23 <brandonvinson23@gmail.com>
2021-08-15 17:33:18 -0500

    controller setup

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project_Management_App.Models;
+using Project_Management_App.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Project_Management_App.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IUserRepository _userRepository;
+        public ProjectController(IProjectRepository projectRepository, IUserRepository userRepository)
+        {
+            _projectRepository = projectRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var user = GetCurrentFirebaseUserId();
+            var projects = _projectRepository.GetAllUserProjects(user);
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var project = _projectRepository.GetProjectById(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public IActionResult AddProject(Project project)
+        {
+            var currentUser = GetCurrentUser();
+            project.UserId = currentUser.Id;
+
+            _projectRepository.AddProject(project);
+            return CreatedAtAction("Get", new { id = project.Id }, project);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Project project)
+        {
+            var currentUser = GetCurrentUser();
+            if (project.UserId != currentUser.Id)
+            {
+                return Unauthorized();
+            }
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            _projectRepository.UpdateProject(project);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _projectRepository.DeleteProject(id);
+            return NoContent();
+        }
+
+        private string GetCurrentFirebaseUserId()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            if (firebaseUserId != null)
+            {
+                return firebaseUserId;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private User GetCurrentUser()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
