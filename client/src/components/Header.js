20b8a056beb58b92b299f1100d87e36b207bccb2
bvinson23 <brandonvinson23@gmail.com>
2021-08-15 16:26:43 -0500

    header all setup, needs styling help

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,44 +5,44 @@ import "../App.css";
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from "reactstrap";
 
 const Header = ({ isLoggedIn }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    return (
-        <>
-            <div>
-                <Navbar dark expand="md">
-                    <NavbarBrand tag={RRNavLink} to="/">Home</NavbarBrand>
-                    <NavbarToggler onClick={toggle} />
-                    <Collapse isOpen={isOpen} navbar>
-                        <Nav className="mr-auto" navbar>
-                            {isLoggedIn &&
-                                <>
-                                    <NavItem>
-                                        <NavLink tag={RRNavLink} to="/projects">Projects</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <a aria-current="page" className="nav-link"
-                                            style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
-                                    </NavItem>
-                                </>
-                            }
-                            {!isLoggedIn &&
-                                <>
-                                    <NavItem>
-                                        <NavLink tag={RRNavLink} to="/login">Login</NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavItem tag={RRNavLink} to="/register">Register</NavItem>
-                                    </NavItem>
-                                </>
-                            }
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand tag={RRNavLink} to="/">Home</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              {isLoggedIn &&
+                <>
+                  <NavItem>
+                    <NavLink tag={RRNavLink} to="/projects">Projects</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <a aria-current="page" className="nav-link"
+                      style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                  </NavItem>
+                </>
+              }
+              {!isLoggedIn &&
+                <>
+                  <NavItem>
+                    <NavLink tag={RRNavLink} to="/login">Login</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavItem tag={RRNavLink} to="/register">Register</NavItem>
+                  </NavItem>
+                </>
+              }
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    </>
+  );
+};
 
 export default Header;
